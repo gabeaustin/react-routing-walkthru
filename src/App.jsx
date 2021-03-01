e0226ab9ecaf3e6a231f439c4c89f914fdede6ac
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-01 16:53:11 -0600

    added route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <h1></h1>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
