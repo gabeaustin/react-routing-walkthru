712b4c3870e0329f9b3a0ed830d87b55e3db57bb
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 08:36:01 -0600

    update userdetails page

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const UserDetails = () => {
     const { userid } = useParams();
+    const [user, setUser] = useState(null); 
+
+    useEffect(() => {
+        fetch(`http://jsonplaceholder.typicode.com/users/${userid}`)
+        .then(res => res.json())
+        .then(user => setUser(user));
+    }, []);
+
 
     return (
         <main className="container">
             <section className="row mt-3">
                 <div className="col-12">
-                    <h1 className="text-center">UserDetails Page for {userid}</h1>
+                    <h4 className="text-center">Details for</h4>
+                    <h1 className="text-center">{user?.name}</h1>
+                    <Link to="/users" className="btn btn-secondary">Go Back</Link>
                 </div>
             </section>
         </main>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(users => setUsers(users));
+    }, []);
+
     return (
         <main className="container">
-            <section className="row mt-3">
-                <div className="col-12">
-                    <h1 className="text-center">Users Page</h1>
+            <section className="row justify-content-center mt-3">
+                <div className="col-md-6">
+                    <ul className="list-group">
+                        {users.map(user => (
+                            <li key={`user-item-${user.id}`} 
+                                className="list-group-item d-flex justify-content-between align-items-center">
+                                {user.username}
+                                <Link to={`/users/${user.id}`} className="btn btn-primary">Read Profile</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    
                 </div>
             </section>
         </main>
