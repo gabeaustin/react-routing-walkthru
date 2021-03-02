d6e257bd1f28d50252ff6e4f532783b059026dff
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 07:23:37 -0600

    added About component and routed to it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
     return (
@@ -12,7 +13,7 @@ const App = () => {
                 </Route>
 
                 <Route exact path="/about">
-                    {() => <h1>About Page</h1>}
+                    <About />
                 </Route>
                 
                 <Route exact path="/projects">
